3bca5a72bf2cd46a8edaee174bc58f311d15d53a
Stefamnus <stefanusrafael1207@gmail.com>
2026-09-21 16:38:05 +0000

    Menambahkan program penghitung total hasil panen

--- panen.py
+++ panen.py
@@ -2,17 +2,11 @@ def hitung_total_panen(jumlah_kg, harga_per_kg):
     return jumlah_kg * harga_per_kg
 
 
-def hitung_diskon(total, persentase_diskon):
-    return total - (total * persentase_diskon / 100)
-
-
 jumlah_panen = 500
 harga_per_kg = 7000
 
 total = hitung_total_panen(jumlah_panen, harga_per_kg)
-total_setelah_diskon = hitung_diskon(total, 10)
 
 print("Jumlah panen:", jumlah_panen, "kg")
 print("Harga per kg: Rp", harga_per_kg)
 print("Total hasil panen: Rp", total)
-print("Total setelah diskon 10%: Rp", total_setelah_diskon)
